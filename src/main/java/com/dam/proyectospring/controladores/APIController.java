4c0823fb66f631e6701d273ed9f2ee630a8474ba
Epso1 <cesaralvarorubert@gmail.com>
2024-02-14 20:02:01 +0100

    César, primer commit

--- src/main/java/com/dam/proyectospring/controladores/APIController.java
+++ src/main/java/com/dam/proyectospring/controladores/APIController.java
@@ -2,7 +2,6 @@ package com.dam.proyectospring.controladores;
 
 import com.dam.proyectospring.modelos.Piloto;
 import com.dam.proyectospring.servicios.PilotoServicio;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
